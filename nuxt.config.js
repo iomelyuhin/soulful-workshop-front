54072bae6f1f7d330b8fe4cb853f866acc554101
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2021-02-16 20:49:07 +0300

    fix puglint error

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ["nuxt-puglint-module"],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxtjs/style-resources"],
