4e602882d2b2b642520f84cd1f11188e7642b776
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2021-02-17 14:50:11 +0300

    fix lint warnings

--- components/myButton.vue
+++ components/myButton.vue
@@ -1,7 +1,8 @@
 <template>
-  <button class="font-bold py-2 px-4 text-white" 
-          :class="btnClass" 
-          @click.prevent="$emit('clickAction', text)">
+  <button 
+  class="font-bold py-2 px-4 text-white"
+  :class="btnClass" 
+  @click.prevent="$emit('clickAction', text)">
     <slot>{{ text }}</slot>
   </button>
 </template>
