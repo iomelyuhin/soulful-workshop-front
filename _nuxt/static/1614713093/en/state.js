7dd1327ba2f3c3b0d09c0503cc0b8fd5e300357d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-02 19:25:08 +0000

    Deploy iomelyuhin/soulful-workshop-front to github.com/iomelyuhin/soulful-workshop-front.git:gh-pages

---
+++ _nuxt/static/1614713093/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsoulful-workshop-front\u002F_nuxt\u002Fstatic\u002F1614713093",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen",config:{app:{basePath:"\u002Fsoulful-workshop-front\u002F",assetsPath:"\u002Fsoulful-workshop-front\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1614713093/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fcart","\u002Fcart","\u002Fcatalog","\u002Fen\u002Fcatalog","\u002Fen"]})
